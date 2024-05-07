3bce492333a34eca09f31b06e70913dc1d61f13a
Matthew Manela <mmanela@users.noreply.github.com>
2024-05-07 16:18:09 -0400

    add dep

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/sourcegraph/conc/pool"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+	p := pool.NewWithResults[int]().WithMaxGoroutines(8)
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for _, result := range nums {
+
+		p.Go(func() int {
+			return result
+		})
+	}
+	res := p.Wait()
+	fmt.Printf("res: %v\n", res)
 }
