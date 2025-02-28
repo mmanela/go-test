9455d7835d474f0202102126ce962f00605c2e2f
Matthew Manela <mmanela@users.noreply.github.com>
2025-02-28 14:30:14 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	fmt.Println("Hello, world!")
 	p := pool.NewWithResults[int]().WithMaxGoroutines(8)
-	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
 	for _, result := range nums {
 
 		p.Go(func() int {
